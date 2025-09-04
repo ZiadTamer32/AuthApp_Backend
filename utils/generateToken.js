bfb21d2414c691aa5b7e026bd55b056a9a79a36d
Ziad Tamer <ziadtamer0543@gmail.com>
2025-09-04 20:25:59 +0300

    Fix sameSite Cookie

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken";
 function generateToken(userId, res) {
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
+    expiresIn: process.env.JWT_EXPIRES_IN,
   });
   res.cookie("Auth-Token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
-    maxAge: 90 * 24 * 60 * 60 * 1000
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+    maxAge: 90 * 24 * 60 * 60 * 1000,
   });
   return token;
 }
